ac1c68a41a32a5bf4fe5ed1a20986e124b514ef2
Felipe Rodriguez <firerodriguez24@gmail.com>
2022-08-18 14:52:27 -0400

    acortar la funcion de carculo de edad ()

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 let nombre = "Juan Perez";
-let edad = calcularEdad(1986);
+let edad = calcularEdad(1996);
 console.log("Hola " + nombre);
 console.log(nombre +"tiene" + edad+ "años" );
 
 function calcularEdad (anioNacimiento){
-    const fechaActual = new Date();
-    const anioActual = fechaActual.getFullYear();
-    const edad = anioActual - anioNacimiento;
-    return edad;
+    return new Date().getFullYear()-anioNacimiento;
 }
