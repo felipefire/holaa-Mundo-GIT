248f4b4a009ec101e4f4eaf137040f22e761f868
Felipe Rodriguez <firerodriguez24@gmail.com>
2022-08-25 14:02:10 -0400

    se creo la funcion calcularSalario

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let nombre = "Juan Perez";
 let edad = calcularEdad(1996);
 console.log("Hola " + nombre);
 console.log(nombre +"tiene" + edad+ "años" );
+console.log("se le pagara:"+calcularSalario(16));
 
 function calcularEdad (anioNacimiento){
     return new Date().getFullYear()-anioNacimiento;
+}
+function calcularSalario(horasTrabajadas){
+    return horasTrabajadas * 20000
 }
